c995e460483ad57b36e0427f6abec9167e2713e5
Michael Wydler <michael.wydler@googlemail.com>
2013-03-15 18:01:59 +0100

    Add basic template and events.

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
-	io = require('socket.io').listen(server);
+	io = require('socket.io').listen(server),
+	os = require('os');
 
 server.listen(8080);
 
@@ -12,9 +13,31 @@ app.get('/', function (req, res) {
 });
 
 io.sockets.on('connection', function (socket) {
-	socket.emit('news', { hello: 'world' });
-	socket.on('name', function (name, fn) {
-		console.log(name);
-		fn('name received.');
+	procs = new Array();
+
+	socket.emit('dashboard', 
+		{
+			hostname: os.hostname(),
+			type: os.type(),
+			platform: os.platform(),
+			arch: os.arch(),
+			release: os.release()
+		}
+	);
+	socket.on('hello', function (data, fn) {
+		procs.push(data);
+		fn(procs);
 	});
+
+	setInterval(function () {
+		socket.emit('system', 
+			{
+				uptime: os.uptime(),
+				loadavg: os.loadavg(),
+				totalmem: os.totalmem(),
+				freemem: os.freemem(),
+				cpus: os.cpus()
+			}
+		);
+	}, 2000);
 });
